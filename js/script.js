ec77ec5920e3b2e538cb70a4447b27d4adea9b63
M <unreality@live.it>
2020-11-20 15:35:51 +0100

    programma funzionante
    no bonus no refactoring

--- js/script.js
+++ js/script.js
@@ -3,46 +3,65 @@
 var bombe = [];
 
 while (bombe.length < 16) {
-  var numeroCasuale100 = numeroCasuale(1, 101);
+  var numeroCasuale100 = numeroCasuale(1, 100);
 
-  var duplicato = controllaSeIlNumeroEsisteGia(numeroCasuale100, bombe);
-  if (duplicato == false) {
+  // I numeri non possono essere duplicati.
+  var verifica = controllaSeIlNumeroEsisteGia(numeroCasuale100, bombe);
+  if (verifica == false) {
 
     bombe.push(numeroCasuale100);
   }
 
 }
-
-console.log('array16: ', bombe);
-
-// I numeri non possono essere duplicati.
-
+console.log('bombe: ', bombe.sort());
 
 
 // In seguito deve chiedere all’utente di inserire 84 volte un numero (uno alla volta),
 // sempre compreso tra 1 e 100.
+var numeroMassimoPossibilita = 100 - 16;
+var tentativi = [];
+var punteggio = 0;
 
+var perso = false;
+while (tentativi.length < numeroMassimoPossibilita && perso == false) {
+  var numeroUtente = parseInt(prompt('inserisci un numer otra 1 e 100'));
+  // L’utente non può inserire più volte lo stesso numero.
+  var controllo = controllaSeIlNumeroEsisteGia(numeroUtente, tentativi);
+    console.log('controllo: ', controllo);
+  var controlloBombe = controllaSeIlNumeroEsisteGia(numeroUtente, bombe)
+    console.log('controlloBombe: ', controlloBombe);
 
+    // La partita termina quando il giocatore inserisce un numero “vietato”
+    // Se il numero è presente nella lista dei numeri generati, la partita termina,
+  if (controlloBombe == true) {
+    alert('Hai perso');
+    perso = true;
 
-// L’utente non può inserire più volte lo stesso numero.
+    // Se il numero non è presente nella lista dei numeri generati si continua chiedendo all’utente un altro numero.
+    // La partita termina quando raggiunge il numero massimo possibile di numeri consentiti.
+  } else if (controllo == false) {
+    tentativi.push(numeroUtente);
 
+    if (!isNaN(numeroUtente)) {
+      punteggio++
+    }
+  }
 
-
-// Se il numero è presente nella lista dei numeri generati, la partita termina,
-// altrimenti si continua chiedendo all’utente un altro numero.
-
-
-
-// La partita termina quando il giocatore inserisce un numero “vietato”
-// o raggiunge il numero massimo possibile di numeri consentiti.
-
-
+}
+console.log('tentativi: ', tentativi);
 
 // Al termine della partita il software deve comunicare il punteggio,
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
+alert('Punteggio finale: ' + punteggio);
+console.log('punteggio: ', punteggio);
 
 
 
+// BONUS:
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
 
 
 
@@ -57,20 +76,20 @@ console.log('array16: ', bombe);
 //-------------------------------------------------------------------------------------
 
 function numeroCasuale(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 //-------------------------------------------------------------------------------------
 
 function controllaSeIlNumeroEsisteGia(numero, array) {
-  var ePresente = false;
+  var trovato = false;
   for (var i = 0; i < array.length; i++) {
 
     if (numero == array[i]) {
-      ePresente = true;
+      trovato = true;
     }
   }
-  if (ePresente == true) {
+  if (trovato == true) {
     return true;
   } else {
     return false;
@@ -78,32 +97,3 @@ function controllaSeIlNumeroEsisteGia(numero, array) {
 }
 
 //-------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//----------------------------------------------------------------------------------------------------------------------------
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 =>  tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
