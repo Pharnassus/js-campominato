c76f83feef39c02cd2d6d610a1440a3df5072189
M <unreality@live.it>
2020-11-22 16:31:58 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -2,8 +2,34 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 var bombe = [];
 
+//------------------------------------------------------------------------------------------------------------------------------------
+//BONUS
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+var livello;
+var difficolta;
+
+livello = prompt('Scegli la difficoltà: 0, 1 o 2')
+
+switch (livello) {
+  // con difficoltà 2 => tra 1 e 50
+  case '2':
+  difficolta = 50;
+  break;
+  // con difficoltà 1 =>  tra 1 e 80
+  case '1':
+  difficolta = 80;
+  break;
+  // con difficoltà 0 => tra 1 e 100
+  default:
+  difficolta = 100;
+}
+
+numeroMassimoPossibilita = difficolta - 16;
+//------------------------------------------------------------------------------------------------------------------------------------
+
+
 while (bombe.length < 16) {
-  var numeroCasuale100 = numeroCasuale(1, 100);
+  var numeroCasuale100 = numeroCasuale(1, difficolta);
 
   // I numeri non possono essere duplicati.
   var verifica = controllaSeIlNumeroEsisteGia(numeroCasuale100, bombe);
@@ -18,13 +44,13 @@ console.log('bombe: ', bombe.sort());
 
 // In seguito deve chiedere all’utente di inserire 84 volte un numero (uno alla volta),
 // sempre compreso tra 1 e 100.
-var numeroMassimoPossibilita = 100 - 16;
+var numeroMassimoPossibilita;
 var tentativi = [];
 var punteggio = 0;
 
 var perso = false;
 while (tentativi.length < numeroMassimoPossibilita && perso == false) {
-  var numeroUtente = parseInt(prompt('inserisci un numer otra 1 e 100'));
+  var numeroUtente = parseInt(prompt('inserisci un numero tra 1 e ' + difficolta));
   // L’utente non può inserire più volte lo stesso numero.
   var controllo = controllaSeIlNumeroEsisteGia(numeroUtente, tentativi);
     console.log('controllo: ', controllo);
@@ -54,17 +80,6 @@ console.log('tentativi: ', tentativi);
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
 alert('Punteggio finale: ' + punteggio);
 console.log('punteggio: ', punteggio);
-
-
-
-// BONUS:
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 =>  tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
-
-
-
 
 
 
